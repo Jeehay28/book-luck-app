722dfd9aa5a0b2e6d619852891607747c9100ebd
jeehay <sophiepark528@gmail.com>
2024-11-26 23:40:30 +0900

    Add default home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,14 +14,11 @@ class HomeScreen extends StatelessWidget {
             Container(
               height: 47,
             ),
-
             Container(
-              // width: 360,
               height: 58,
-              // color: Colors.blue, // You can customize the color here
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: Colors.blue, // Border color
+                  color: Colors.transparent, // Border color
                   width: 1, // Border width
                 ),
                 color: Colors.transparent, // Transparent background
@@ -29,131 +26,121 @@ class HomeScreen extends StatelessWidget {
                     Radius.circular(0)), // Optional: rounded corners
               ),
               child: Center(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment
-                          .center, // Center the Row horizontally
-                      crossAxisAlignment: CrossAxisAlignment
-                          .center, // Center the Row vertically
-                      children: [
-                    SizedBox(width: 20),
-                    Container(
-                      // width: 113,
-                      height: 34,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue, // Border color
-                          width: 1, // Border width
-                        ),
-                        color: Colors.transparent, // Transparent background
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(0)), // Optional: rounded corners
-                      ),
-                      child: Image.asset(
-                        'assets/images/bookluck.png', // Replace with your image path
-                        fit: BoxFit
-                            .contain, // Adjust how the image fits in the container
-                      ),
-                    ),
-                    SizedBox(width: 144),
-                    Container(
-                      width: 43,
-                      height: 34,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue, // Border color
-                          width: 1, // Border width
-                        ),
-                        color: Colors.transparent, // Transparent background
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(0)), // Optional: rounded corners
-                      ),
+                  child: Container(
+                      width: 328,
                       child: Row(
-                        mainAxisSize: MainAxisSize
-                            .min, // Ensure the Row takes only as much space as needed
-                        crossAxisAlignment: CrossAxisAlignment
-                            .center, // Align items vertically centered// Align the content in the center
-                        children: [
-                          Image.asset(
-                            'assets/images/mail.png',
-                            fit: BoxFit.contain,
-                            width: 24,
-                          ),
-                          SizedBox(width: 7),
-                          Text(
-                            '1',
-                            style: TextStyle(
-                              color: Color(0xFF303030), // Text color
-                              fontSize: 14, // Font size
-                              fontWeight:
-                                  FontWeight.bold, // Optional: Make it bold
+                          mainAxisAlignment: MainAxisAlignment
+                              .spaceBetween, // Center the Row horizontally
+                          crossAxisAlignment: CrossAxisAlignment
+                              .center, // Center the Row vertically
+                          children: [
+                            SizedBox(width: 20),
+                            Container(
+                              // width: 113,
+                              height: 34,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.transparent, // Border color
+                                  width: 1, // Border width
+                                ),
+                                color: Colors
+                                    .transparent, // Transparent background
+                                borderRadius: BorderRadius.all(Radius.circular(
+                                    0)), // Optional: rounded corners
+                              ),
+                              child: Image.asset(
+                                'assets/images/bookluck.png', // Replace with your image path
+                                fit: BoxFit
+                                    .contain, // Adjust how the image fits in the container
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ])),
+                            SizedBox(width: 144),
+                            Container(
+                              width: 43,
+                              height: 34,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.transparent, // Border color
+                                  width: 1, // Border width
+                                ),
+                                color: Colors
+                                    .transparent, // Transparent background
+                                borderRadius: BorderRadius.all(Radius.circular(
+                                    0)), // Optional: rounded corners
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize
+                                    .min, // Ensure the Row takes only as much space as needed
+                                crossAxisAlignment: CrossAxisAlignment
+                                    .center, // Align items vertically centered// Align the content in the center
+                                children: [
+                                  Image.asset(
+                                    'assets/images/mail.png',
+                                    fit: BoxFit.contain,
+                                    width: 24,
+                                  ),
+                                  SizedBox(width: 7),
+                                  Text(
+                                    '1',
+                                    style: TextStyle(
+                                      color: Color(0xFF303030), // Text color
+                                      fontSize: 14, // Font size
+                                      fontWeight: FontWeight
+                                          .bold, // Optional: Make it bold
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ]))),
             ),
-            // SizedBox(height: 16), // Add space between the two boxes
-            // Second Box
             Container(
-              // width: 360,
-              height: 122,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue, // Border color
-                  width: 1, // Border width
+                height: 122,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.transparent, // Border color
+                    width: 1, // Border width
+                  ),
+                  color: Colors.transparent, // Transparent background
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(0)), // Optional: rounded corners
                 ),
-                color: Colors.transparent, // Transparent background
-                borderRadius: BorderRadius.all(
-                    Radius.circular(0)), // Optional: rounded corners
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisSize:
-                      MainAxisSize.min, // Minimize space between items
-                  children: [
-                    Image.asset(
-                      'assets/images/warning.png', // Replace with your image path
-                      width: 50, // Set the width of the image
-                      height: 50, // Set the height of the image
-                      fit: BoxFit.contain, // Adjust image fit if needed
-                    ),
-                    SizedBox(height: 10), // Add space between image and text
-                    Text(
-                      '업데이트된 명언이 없습니다. \n업데이트 전까지 조그만 기다려주세요!',
-                      style: TextStyle(
-                        color: Color.fromRGBO(48, 48, 48, 0.6),
-                        fontSize: 16,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
 
-            // 360 * 122
-            // Container(
-            //   // width: 360,
-            //   height: 122,
-            //   // color: Colors.green,
-            //   decoration: BoxDecoration(
-            //     border: Border.all(
-            //       color: Colors.blue, // Border color
-            //       width: 1, // Border width
-            //     ),
-            //     color: Colors.transparent, // Transparent background
-            //     borderRadius: BorderRadius.all(
-            //         Radius.circular(0)), // Optional: rounded corners
-            //   ),
-            //   child: Center(
-            //     child: Text(
-            //       'Second Box',
-            //       style: TextStyle(color: Colors.white, fontSize: 16),
-            //     ),
-            //   ),
-            // ),
-
+                    fixedSize: Size(320, 240),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        side: BorderSide(
+                            color: Color.fromRGBO(48, 48, 48, 0.12), width: 1)),
+                    elevation:
+                        0, // Remove shadow to mimic semi-transparent background better
+                  ),
+                  onPressed: () {},
+                  child: Center(
+                    child: Column(
+                      mainAxisSize:
+                          MainAxisSize.min, // Minimize space between items
+                      children: [
+                        Image.asset(
+                          'assets/images/warning.png', // Replace with your image path
+                          width: 24, // Set the width of the image
+                          height: 24, // Set the height of the image
+                          fit: BoxFit.contain, // Adjust image fit if needed
+                        ),
+                        Text(
+                          '업데이트된 명언이 없습니다. \n업데이트 전까지 조그만 기다려주세요!',
+                          style: TextStyle(
+                            color: Color.fromRGBO(48, 48, 48, 0.6),
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
             // 360 *252
             Container(
               // width: 360,
@@ -161,7 +148,7 @@ class HomeScreen extends StatelessWidget {
               // color: Colors.green,
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: Colors.blue, // Border color
+                  color: Colors.transparent, // Border color
                   width: 1, // Border width
                 ),
                 color: Colors.transparent, // Transparent background
@@ -199,25 +186,43 @@ class HomeScreen extends StatelessWidget {
 
             // 360 * 70
             Container(
-              // width: 360,
-              height: 70,
-              // color: Colors.green,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue, // Border color
-                  width: 1, // Border width
+                // width: 360,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.transparent, // Border color
+                    width: 1, // Border width
+                  ),
+                  color: Colors.transparent, // Transparent background
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(0)), // Optional: rounded corners
                 ),
-                color: Colors.transparent, // Transparent background
-                borderRadius: BorderRadius.all(
-                    Radius.circular(0)), // Optional: rounded corners
-              ),
-              child: Center(
-                child: Text(
-                  'Second Box',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-              ),
-            ),
+                child: Column(children: [
+                  Center(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromRGBO(
+                              48, 48, 48, 0.24), // Button background color
+                          fixedSize: Size(320, 20), // Button size
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(8)), // Rounded corners
+                          ),
+                        ),
+                        onPressed: () {
+                          // Action for the button
+                        },
+                        child: null),
+                  ),
+                  Container(
+                      width: 320, // Set the width for the container
+                      height: 22,
+                      child: Center(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [Text("0분"), Text("30분"), Text("1시간")]),
+                      ))
+                ])),
 
             // 360 * 57
             Container(
@@ -225,7 +230,7 @@ class HomeScreen extends StatelessWidget {
               height: 57, // Height of the container
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: Colors.blue, // Border color
+                  color: Colors.transparent,
                   width: 1, // Border width
                 ),
                 color: Colors.transparent, // Transparent background
@@ -273,7 +278,7 @@ class HomeScreen extends StatelessWidget {
               height: 80, // Parent container height
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: Colors.blue, // Border color
+                  color: Colors.transparent,
                   width: 1, // Border width
                 ),
                 color: Colors.transparent, // Transparent background
@@ -328,11 +333,11 @@ class HomeScreen extends StatelessWidget {
             // 360 * 104
             Container(
               // width: 360,
-              height: 104,
+              height: 70,
               // color: Colors.green,
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: Colors.blue, // Border color
+                  color: Colors.transparent,
                   width: 1, // Border width
                 ),
                 color: Colors.transparent, // Transparent background
@@ -340,11 +345,61 @@ class HomeScreen extends StatelessWidget {
                     Radius.circular(0)), // Optional: rounded corners
               ),
               child: Center(
-                child: Text(
-                  'Second Box',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
+                  child: Container(
+                height: 50,
+                width: 320,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceAround, // Center the Row horizontally
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset('assets/images/home.png',
+                            height: 24), // Image on top
+                        // Space between image and text
+                        Text("홈",
+                            style:
+                                TextStyle(fontSize: 12)), // Text at the bottom
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset('assets/images/bookshelf.png',
+                            height: 24), // Image on top
+                        // Space between image and text
+                        Text("책장",
+                            style:
+                                TextStyle(fontSize: 12)), // Text at the bottom
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset('assets/images/feed.png',
+                            height: 24), // Image on top
+                        // Space between image and text
+                        Text("피드",
+                            style:
+                                TextStyle(fontSize: 12)), // Text at the bottom
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset('assets/images/my-page.png',
+                            height: 24), // Image on top
+                        // Space between image and text
+                        Text("마이페이지",
+                            style:
+                                TextStyle(fontSize: 12)), // Text at the bottom
+                      ],
+                    ),
+                  ],
                 ),
-              ),
+              )),
             ),
           ],
         ),
