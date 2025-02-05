33e528f1ac47bb74ff63895c38a7720deeaa81dd
jeehay <sophiepark528@gmail.com>
2025-02-06 02:05:01 +0900

    Change selected button color change

--- lib/screens/bookshelf_screen.dart
+++ lib/screens/bookshelf_screen.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class BookshelfScreen extends StatelessWidget {
+class BookshelfScreen extends StatefulWidget {
+  @override
+  _BookshelfScreenState createState() => _BookshelfScreenState();
+}
+
+class _BookshelfScreenState extends State<BookshelfScreen> {
+  String selectedCategory = "전체";
+
   @override
   Widget build(BuildContext context) {
     final bodyHeight = MediaQuery.of(context).size.height -
@@ -139,7 +147,12 @@ class BookshelfScreen extends StatelessWidget {
                                     padding: const EdgeInsets.only(
                                         right: 8.0), // Spacing between buttons
                                     child: OutlinedButton(
-                                      onPressed: () {}, // Add your action here
+                                      onPressed: () {
+                                        setState(() {
+                                          // Select the clicked category and deselect the previous one
+                                          selectedCategory = category;
+                                        });
+                                      }, // Add your action here
                                       style: OutlinedButton.styleFrom(
                                         shape: RoundedRectangleBorder(
                                           borderRadius: BorderRadius.circular(
@@ -150,19 +163,32 @@ class BookshelfScreen extends StatelessWidget {
                                             horizontal: 0.0222 * bodyWidth,
                                             vertical: 0.0099 * bodyHeight),
                                         side: BorderSide(
-                                          color: Color.fromRGBO(
-                                              48, 48, 48, 0.08), // Border color
+                                          color: selectedCategory == category
+                                              ? Colors
+                                                  .transparent // No border if selected
+                                              : Color.fromRGBO(48, 48, 48,
+                                                  0.08), // Border color
                                           width: 1, // Border width
                                         ),
+                                        backgroundColor: selectedCategory ==
+                                                category
+                                            ? Color(
+                                                0xFF303030) // Background color when selected
+                                            : Colors
+                                                .transparent, // Default background color
                                       ),
                                       child: Text(
                                         category,
                                         style: TextStyle(
-                                          color: Color(0xFF303030),
+                                          color: selectedCategory == category
+                                              ? Colors
+                                                  .white // Text color when selected
+                                              : Color(
+                                                  0xFF303030), // Default text color
                                           fontFamily: 'SUITVariable',
                                           fontSize:
                                               0.0333 * bodyWidth, // Text size
-                                          fontWeight: FontWeight.w600,
+                                          fontWeight: FontWeight.w700,
                                           height: 1.5,
                                           letterSpacing: -0.32,
                                         ),
@@ -176,12 +202,33 @@ class BookshelfScreen extends StatelessWidget {
 
                 // Container 4 with specific height
                 Container(
-                  height: 0.0733 * bodyHeight, // 8% of the screen height
-                  color: Colors.orange,
-                  child: Center(
-                    child: Text('전체 10'),
-                  ),
-                ),
+                    height: 0.0733 * bodyHeight, // 8% of the screen height
+                    // color: Colors.orange,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 0.0556 * bodyWidth,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment
+                            .spaceBetween, // Space between items
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            selectedCategory.isNotEmpty
+                                ? selectedCategory
+                                : '전체',
+                            style: TextStyle(
+                              color: Color(0xFF303030),
+                              fontSize: 0.0389 * bodyWidth,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'SUITVariable',
+                              height: 1.5,
+                              letterSpacing: -0.32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
 
                 // Container 5 with specific height
                 Container(
