aff25696f29fea1bd82a6c26f52eafeeaab2d5d7
Jeehay28 <sophiepark528@gmail.com>
2025-04-29 00:41:19 +0900

    Update dialogs for reading

--- lib/screens/bookshelf_screen.dart
+++ lib/screens/bookshelf_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:book_luck_app_demo/widgets/BookListContainer.dart';
